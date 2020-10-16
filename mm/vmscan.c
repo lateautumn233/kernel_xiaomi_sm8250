b0e28aa61677c6b449634565f5d39804b9bd05ff
Adam W. Willis <return.of.octobot@gmail.com>
2020-10-15 23:48:03 -0400

    mm: vmscan: Define default swappiness for VBSWAP
    Until I can come up with a clean way to dynamically set swappiness
    based on available RAM, we'll default to assuming the most common
    common device variant (8GiB) and set default swappiness to 160 when
    VBSWAP is enabled.
    
    Now that the corresponding sysfs node has been refactored, we no
    longer need to worry about userspace overwriting this value.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -178,7 +178,11 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
+#ifdef CONFIG_VBSWAP
 int vm_swappiness = 160;
+#else
+int vm_swappiness = 60;
+#endif
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
