277429c4825d565a667a9537bc8af23b58d967c7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-24 00:11:33 +0100

    drivers: usb: Import Xiaomi changes

--- drivers/usb/typec/class.c
+++ drivers/usb/typec/class.c
@@ -1381,6 +1381,9 @@ void typec_set_pwr_opmode(struct typec_port *port,
 	if (port->pwr_opmode == opmode)
 		return;
 
+	if (opmode > TYPEC_PWR_MODE_PD)
+		return;
+
 	port->pwr_opmode = opmode;
 	sysfs_notify(&port->dev.kobj, NULL, "power_operation_mode");
 	kobject_uevent(&port->dev.kobj, KOBJ_CHANGE);
