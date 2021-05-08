8e04bad625dbf8d9b78ea45756612fc23d76afd4
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-08 11:28:10 -0500

    fs: cleanup
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1229,13 +1229,11 @@ retry_flush_dents:
 		cond_resched();
 		goto retry_flush_quotas;
 	}
-
+retry_flush_nodes:
 	down_write(&sbi->node_write);
 
 	if (get_pages(sbi, F2FS_DIRTY_NODES)) {
 		up_write(&sbi->node_write);
-		up_write(&sbi->node_change);
-		f2fs_unlock_all(sbi);
 		atomic_inc(&sbi->wb_sync_req[NODE]);
 		err = f2fs_sync_node_pages(sbi, &wbc, false, FS_CP_NODE_IO);
 		atomic_dec(&sbi->wb_sync_req[NODE]);
@@ -1245,7 +1243,7 @@ retry_flush_dents:
 			return err;
 		}
 		cond_resched();
-		goto retry_flush_quotas;
+		goto retry_flush_nodes;
 	}
 
 	/*
--- fs/f2fs/debug.c
+++ fs/f2fs/debug.c
@@ -11,7 +11,6 @@
 #include <linux/fs.h>
 #include <linux/backing-dev.h>
 #include <linux/f2fs_fs.h>
-#include <linux/proc_fs.h>
 #include <linux/blkdev.h>
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
@@ -26,7 +25,6 @@ static DEFINE_MUTEX(f2fs_stat_mutex);
 #ifdef CONFIG_DEBUG_FS
 static struct dentry *f2fs_debugfs_root;
 #endif
-extern struct proc_dir_entry *f2fs_proc_root;
 
 /*
  * This function calculates BDF of every segments
@@ -61,6 +59,7 @@ void f2fs_update_sit_info(struct f2fs_sb_info *sbi)
 		si->avg_vblocks = 0;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static void update_general_status(struct f2fs_sb_info *sbi)
 {
 	struct f2fs_stat_info *si = F2FS_STAT(sbi);
@@ -482,10 +481,8 @@ static int stat_show(struct seq_file *s, void *v)
 	return 0;
 }
 
-#ifdef CONFIG_DEBUG_FS
 DEFINE_SHOW_ATTRIBUTE(stat);
 #endif
-
 int f2fs_build_stats(struct f2fs_sb_info *sbi)
 {
 	struct f2fs_super_block *raw_super = F2FS_RAW_SUPER(sbi);
@@ -551,16 +548,10 @@ void __init f2fs_create_root_stats(void)
 	debugfs_create_file("status", S_IRUGO, f2fs_debugfs_root, NULL,
 			    &stat_fops);
 #endif
-	if (f2fs_proc_root)
-		proc_create_single_data("status", S_IRUGO, f2fs_proc_root,
-				stat_show, NULL);
 }
 
 void f2fs_destroy_root_stats(void)
 {
-	if (f2fs_proc_root)
-		remove_proc_entry("status", f2fs_proc_root);
-
 #ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(f2fs_debugfs_root);
 	f2fs_debugfs_root = NULL;
