b4f1a465fc0293ddc30b0b6cea91dd083301562e
root <799620521@qq.com>
2021-05-08 15:46:51 +0800

    sysctl: Disallow memory randomization write permission

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1410,7 +1410,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "randomize_va_space",
 		.data		= &randomize_va_space,
 		.maxlen		= sizeof(int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
 	},
 #endif
