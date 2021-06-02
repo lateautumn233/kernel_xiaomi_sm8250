bd91c927b4fffd5d0bb7ae64e6b21d880b982ca9
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-06-03 00:06:34 +0800

    erofs: clean up file headers & footers
     - Remove my outdated misleading email address;
    
     - Get rid of all unnecessary trailing newline by accident.
    
    Link: https://lore.kernel.org/r/20210602160634.10757-1-xiang@kernel.org
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -2,7 +2,6 @@
 /*
  * Copyright (C) 2018 HUAWEI, Inc.
  *             https://www.huawei.com/
- * Created by Gao Xiang <gaoxiang25@huawei.com>
  */
 #include "zdata.h"
 #include "compress.h"
@@ -1372,4 +1371,3 @@ const struct address_space_operations z_erofs_aops = {
 	.readpage = z_erofs_readpage,
 	.readpages = z_erofs_readpages,
 };
-
