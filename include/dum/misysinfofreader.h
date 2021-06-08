b5999910a5a8e1d41742cf4be766e014159a4685
KyuoFoxHuyu <88403766@qq.com>
2021-06-09 01:39:33 +0800

    treewide: cleaned-up xiaomi changes
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- include/dum/misysinfofreader.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MISYSINFOFREADER_H__
-#define __MISYSINFOFREADER_H__
-
-static __always_inline void update_misysinfo_jiffies(void) {}
-static __always_inline void misysinfo_update_slowpath(u64 time_stamp_ns, u64 time_ns) {}
-
-#endif /*__MISYSINFOFREADER_H__*/
