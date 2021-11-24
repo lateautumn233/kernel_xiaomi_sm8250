fd935ab410addf7c034720d03cc3f346e7c52255
lateautumn <3487467850@qq.com>
2021-11-24 20:05:04 +0800

    Revert "devfreq_boost: Add support for boosting CPU_LLCC_BW device"
    This reverts commit 72ab9c467ed2b4423619ae25520d2267ed14cdde.

--- kernel/fork.c
+++ kernel/fork.c
@@ -2376,7 +2376,6 @@ long _do_fork(unsigned long clone_flags,
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 50);
 	}
 
 	/*
